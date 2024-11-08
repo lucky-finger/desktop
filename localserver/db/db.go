5cb47da0c72bd9b7e4e55faca06b6167c16a4e58
晓鑫 <sulinxin66@gmail.com>
2024-11-09 00:09:18 +0800

    refactor: ♻️ sqlite去除gcc

--- localserver/db/db.go
+++ localserver/db/db.go
@@ -2,16 +2,30 @@ package db
 
 import (
 	"fmt"
-	"gorm.io/driver/sqlite"
+	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"os"
 )
 
 // _db 数据库连接
 var _db *gorm.DB
 
+type writer struct {
+}
+
+func (w *writer) Printf(s string, i ...interface{}) {
+	_, _ = fmt.Fprintln(os.Stderr, fmt.Sprintf(s, i...))
+}
+
 // Load 初始化数据库
 func Load(isDev bool, dbFile string) (err error) {
-	if _db, err = gorm.Open(sqlite.Open(fmt.Sprintf("file:%s?_auto_vacuum=1&_synchronous=0", dbFile)), &gorm.Config{}); err != nil {
+	if _db, err = gorm.Open(sqlite.Open(fmt.Sprintf("file:%s?_auto_vacuum=1&_synchronous=0", dbFile)), &gorm.Config{
+		Logger: logger.New(&writer{}, logger.Config{
+			Colorful: true,
+			LogLevel: logger.Info,
+		}),
+	}); err != nil {
 		return err
 	}
 	if isDev {
