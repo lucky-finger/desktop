a6d416635704507ac425982e406fd74301102261
夏夜 <sulinxin66@gmail.com>
2024-11-08 17:43:02 +0800

    refactor: ♻️ 抽离electron基本文件结构

--- src/main/tools/windows/index.ts
+++ src/main/tools/windows/index.ts
@@ -1,20 +1,66 @@
 import { is } from '@electron-toolkit/utils';
-import { BrowserWindowConstructorOptions } from 'electron';
+import { app, BrowserWindow, BrowserWindowConstructorOptions, shell } from 'electron';
 import { join } from 'path';
 import icon from '../../../../resources/icon.png?asset';
 
+app.enableSandbox();
+
+const electronRenderUrl = process.env['ELECTRON_RENDERER_URL'];
+
+/**
+ * 默认构建参数属性
+ */
 export const defaultWindowConstructorOptions: BrowserWindowConstructorOptions = {
   show: false,
-  frame: false,
-  transparent: true,
+  frame: true,
+  transparent: false,
   autoHideMenuBar: true,
-  titleBarStyle: 'hidden',
+  // titleBarStyle: 'hidden',
   resizable: false,
   backgroundColor: '#00000000',
   ...(process.platform === 'linux' ? { icon } : {}),
   webPreferences: {
     preload: join(__dirname, '../preload/index.js'),
     devTools: is.dev,
-    sandbox: false,
   },
 };
+
+export interface CreateOptions {
+  readyToHidden?: boolean;
+}
+
+export function createWithDefaultOptions(
+  routeName: string,
+  options: BrowserWindowConstructorOptions = {},
+  createOption: CreateOptions = {},
+): BrowserWindow {
+  const bw = new BrowserWindow({
+    ...defaultWindowConstructorOptions,
+    ...options,
+  });
+
+  bw.on('ready-to-show', () => {
+    if (!createOption.readyToHidden) {
+      bw.show();
+    } else if (bw.isVisible()) {
+      bw.hide();
+    }
+
+    if (is.dev) {
+      bw.webContents.openDevTools();
+    }
+  });
+
+  bw.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url);
+    return { action: 'deny' };
+  });
+
+  if (is.dev && electronRenderUrl) {
+    bw.loadURL(electronRenderUrl + '/#/' + routeName);
+  } else {
+    bw.loadFile(join(__dirname, '../renderer/index.html'), { hash: routeName });
+  }
+
+  return bw;
+}
